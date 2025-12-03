e84a40803cd5c0ff716dddd0282d1036e98f1ab1
BumTiger <jbpark03@gmail.com>
2025-12-03 11:05:25 +0900

    add quill style into root compo'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'quill/dist/quill.snow.css'; // For the snow theme
 
 function App() {
   const [count, setCount] = useState(0)
